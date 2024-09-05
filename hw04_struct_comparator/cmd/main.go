2726eb02e44aa9af1c773b4b2493f4aa091ee49b
mar4ehk0 <mar4ehk0@gmail.com>
2024-09-05 18:47:06 +0300

    deleted commented code

--- hw04_struct_comparator/cmd/main.go
+++ hw04_struct_comparator/cmd/main.go
@@ -43,36 +43,3 @@ func main() {
 
 	checker.Check(checkerYear, checkerSize, checkerRate)
 }
-
-// func exampleComparatorYear(a book.Book, b book.Book) {
-// 	cmp := comparator.NewComparator(comparator.Year)
-// 	result := cmp.Compare(a, b)
-
-// 	if result {
-// 		fmt.Printf("Year of book: \"%s\" bigger then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	} else {
-// 		fmt.Printf("Year of book: \"%s\" smaller then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	}
-// }
-
-// func exampleComparatorSize(a book.Book, b book.Book) {
-// 	cmp := comparator.NewComparator(comparator.Size)
-// 	result := cmp.Compare(a, b)
-
-// 	if result {
-// 		fmt.Printf("Size of book: \"%s\" bigger then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	} else {
-// 		fmt.Printf("Size of book: \"%s\" smaller then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	}
-// }
-
-// func exampleComparatorRate(a book.Book, b book.Book) {
-// 	cmp := comparator.NewComparator(comparator.Rate)
-// 	result := cmp.Compare(a, b)
-
-// 	if result {
-// 		fmt.Printf("Rate of book: \"%s\" bigger then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	} else {
-// 		fmt.Printf("Rate of book: \"%s\" smaller then book: \"%s\" \n", a.GetTitle(), b.GetTitle())
-// 	}
-// }

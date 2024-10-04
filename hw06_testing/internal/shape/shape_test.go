c66b3900bc2aea8697d4c0cd700be69bc8c20a02
mar4ehk0 <mar4ehk0@gmail.com>
2024-10-04 20:29:44 +0300

    implemented test for shape

---
+++ hw06_testing/internal/shape/shape_test.go
@@ -0,0 +1,94 @@
+package shape
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanCalculateArea(t *testing.T) {
+	// arrange
+	tests := []struct {
+		name     string
+		shape    Shape
+		expected float64
+	}{
+		{
+			"Circle",
+			NewCircle(5.0),
+			78.53981633974483,
+		},
+		{
+			"Rectangle",
+			NewRectangle(10.0, 5.0),
+			50,
+		},
+		{
+			"Triangle",
+			NewTriangle(8.0, 6.0),
+			24,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// act
+			actual, _ := CalculateArea(tc.shape)
+
+			// assert
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestFailWhenNotShape(t *testing.T) {
+	// arrange
+	tests := []struct {
+		name     string
+		shape    any
+		expected error
+	}{
+		{
+			"Int",
+			10,
+			ErrNotShape,
+		},
+		{
+			"0",
+			0,
+			ErrNotShape,
+		},
+		{
+			"-1",
+			-1,
+			ErrNotShape,
+		},
+		{
+			"nil",
+			nil,
+			ErrNotShape,
+		},
+		{
+			"Custom Struct",
+			struct{ userName string }{userName: "Lorem Ipsum"},
+			ErrNotShape,
+		},
+		{
+			"String",
+			"LoremIpsum",
+			ErrNotShape,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// act
+			_, err := CalculateArea(tc.shape)
+
+			// assert
+			assert.ErrorIs(t, tc.expected, err)
+		})
+	}
+}
---
+++ hw06_testing/internal/shape/triangle.go
@@ -0,0 +1,24 @@
+package shape
+
+import (
+	"fmt"
+)
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func NewTriangle(base, height float64) *Triangle {
+	triangle := &Triangle{base: base, height: height}
+
+	return triangle
+}
+
+func (t *Triangle) Area() float64 {
+	return (t.base * t.height) / 2
+}
+
+func (t *Triangle) String() string {
+	return fmt.Sprintf("Треугольник: основание %f, высота %f", t.base, t.height)
+}

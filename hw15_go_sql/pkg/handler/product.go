0aabace4d81d7f34a3dd976090a4bb1c0bc709ee
mar4ehk0 <mar4ehk0@gmail.com>
2024-12-14 15:31:14 +0300

    implemented handler for product

---
+++ hw15_go_sql/pkg/handler/product.go
@@ -0,0 +1,91 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/mar4ehk0/go/hw15_go_sql/internal/product"
+	"github.com/mar4ehk0/go/hw15_go_sql/pkg/db"
+	"github.com/mar4ehk0/go/hw15_go_sql/pkg/server"
+)
+
+type Handler struct {
+	service *product.Service
+}
+
+func NewHandler(service *product.Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var dto product.CreateDto
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		server.CreateResponse(w, []byte(err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	if len(dto.Name) < 1 || dto.Price < 1 {
+		server.CreateResponse(w, []byte("Not valid values"), http.StatusBadRequest)
+		os.Stdout.Write([]byte("Can't create product, wrong input data.\n"))
+		return
+	}
+
+	pr, err := h.service.Create(dto)
+	if err != nil {
+		if errors.Is(err, db.ErrDBDuplicateKey) {
+			server.CreateResponse(w, []byte("Already exist product"), http.StatusConflict)
+			os.Stdout.Write([]byte("Can't create product, already exist product.\n"))
+			return
+		}
+
+		server.CreateResponse(w, []byte("Something went wrong"), http.StatusInternalServerError)
+		os.Stdout.Write([]byte(err.Error() + "\n"))
+		return
+	}
+
+	data, err := json.Marshal(map[string]int{"id": pr.Id})
+
+	if err != nil {
+		server.CreateResponse(w, []byte("Something went wrong"), http.StatusInternalServerError)
+		os.Stdout.Write([]byte(err.Error()))
+		return
+	}
+
+	server.CreateResponse(w, []byte(data), http.StatusCreated)
+}
+
+func (h *Handler) GetProductById(w http.ResponseWriter, r *http.Request) {
+	idRaw := r.PathValue("id")
+
+	id, err := strconv.Atoi(idRaw)
+	if err != nil {
+		server.CreateResponse(w, []byte("Something went wrong"), http.StatusInternalServerError)
+		return
+	}
+
+	product, err := h.service.GetById(id)
+	if err != nil {
+		if errors.Is(err, db.ErrDBNotFound) {
+			server.CreateResponse(w, []byte("Not found"), http.StatusNotFound)
+			os.Stdout.Write([]byte(err.Error() + "\n"))
+			return
+		}
+		server.CreateResponse(w, []byte("Something went wrong"), http.StatusInternalServerError)
+		os.Stdout.Write([]byte(err.Error() + "\n"))
+		return
+	}
+
+	data, err := json.Marshal(product)
+
+	if err != nil {
+		server.CreateResponse(w, []byte("Something went wrong"), http.StatusInternalServerError)
+		os.Stdout.Write([]byte(err.Error()))
+		return
+	}
+
+	server.CreateResponse(w, []byte(data), http.StatusCreated)
+}

97d91a03a3522795a9cf6328a1247d24b3994c2b
mar4ehk0 <mar4ehk0@gmail.com>
2024-12-21 14:27:55 +0300

    added Handler interface

--- hw15_go_sql/cmd/main.go
+++ hw15_go_sql/cmd/main.go
@@ -16,6 +16,10 @@ import (
 	"github.com/mar4ehk0/go/hw15_go_sql/pkg/server"
 )
 
+type Handler interface {
+	InitializeRoutes(mux *http.ServeMux)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -38,21 +42,26 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	handlers := []Handler{}
+
 	repoProduct := product.NewRepoProduct(dbConnect)
 	serviceProduct := product.NewService(repoProduct)
 	handlerProduct := product.NewHandler(serviceProduct, respService)
-	handlerProduct.InitializeRoutes(mux)
+	handlers = append(handlers, handlerProduct)
 
 	repoUser := user.NewRepoUser(dbConnect)
 	serviceUser := user.NewService(repoUser)
 	handlerUser := user.NewHandler(serviceUser, respService)
-	handlerUser.InitializeRoutes(mux)
+	handlers = append(handlers, handlerUser)
 
 	repoOrder := order.NewRepoOrder(dbConnect)
 	serviceOrder := order.NewService(repoOrder, repoProduct, repoUser)
 	handlerOrder := order.NewHandler(serviceOrder, respService)
+	handlers = append(handlers, handlerOrder)
 
-	handlerOrder.InitializeRoutes(mux)
+	for _, h := range handlers {
+		h.InitializeRoutes(mux)
+	}
 
 	server := &http.Server{
 		Addr:              addr.Connection(),

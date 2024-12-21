284ef9f719fd8c2ee762eb96c090f189c2e5b232
mar4ehk0 <mar4ehk0@gmail.com>
2024-12-21 13:51:25 +0300

    replaced CreateResponse to CreateResponseOK, CreateResponseNoContent, CreateResponseCreated and CreateResponseError

--- hw15_go_sql/pkg/server/response.go
+++ hw15_go_sql/pkg/server/response.go
@@ -11,14 +11,37 @@ func NewResponseService() *ResponseService {
 	return &ResponseService{}
 }
 
-func (r ResponseService) CreateResponse(w http.ResponseWriter, data []byte, httpCode int, err error) {
+func (r ResponseService) CreateResponseOK(w http.ResponseWriter, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write(data)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (r ResponseService) CreateResponseNoContent(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (r ResponseService) CreateResponseCreated(w http.ResponseWriter, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err := w.Write(data)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (r ResponseService) CreateResponseError(w http.ResponseWriter, humanError string, httpCode int, err error) {
 	if err != nil {
 		log.Println(err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(httpCode)
-	_, err = w.Write(data)
+	_, err = w.Write([]byte(humanError))
 	if err != nil {
 		log.Println(err)
 	}
